792d2a62a838d1addb22196ab9fcb64dd2a6e3cd
David Lefrancq <david.lefrancq@gmail.com>
2024-11-26 12:34:27 +0100

    build: cors config

--- src/main.py
+++ src/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
 app = FastAPI()
+
+# Configuration du CORS
+app.add_middleware(
+    CORSMiddleware,
+    # Liste des origines autorisées (vous pouvez ajuster selon vos besoins)
+    allow_origins=["http://localhost:5173", "http://57.128.24.53:8294"],  # Origine de votre app Vue.js
+    allow_credentials=True,
+    allow_methods=["*"],  # Autorise toutes les méthodes HTTP
+    allow_headers=["*"],  # Autorise tous les headers
+)
 
 @app.get("/")
 async def read_root():
